79e46c043f81f508016e7d9dd63e4c572fb29be1
Joshua <jpic@umich.edu>
2022-10-13 16:58:36 -0400

    mpi book dir

---
+++ Karniadkis-Kirby/Ch2/p85.cpp
@@ -0,0 +1,14 @@
+nclude <iostream.h>
+#include <mpi.h>
+
+int main(int argc, char ** argv)
+{
+	int node, totalnodes;
+        MPI_Init(&argc, &argv);
+        MPI_Comm_size(MPI_COMM_WORLD, &totalnodes);
+        MPI_Comm_rank(MPI_COMM_WORLD, &node);
+
+        cout << "Hello from node " << node << " of " << totalnodes << endl;
+
+	MPI_Finalize();
+}
